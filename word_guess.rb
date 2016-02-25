0d8a7eb334316afd7cb4ac0262324f988975fe59
Lisa Rolczynski <lisa.sano@gmail.com>
2016-02-25 14:07:48 -0800

    Added an instance variable and method.
    Instance variable @word gets a random word from an existing word list using a get_word method.
    Tests that a word object can be created and a word randomly assigned to it.

--- word_guess.rb
+++ word_guess.rb
@@ -1,3 +1,16 @@
 class WordGuess 
+  attr_reader :word
+
+  def initialize
+    @word = get_word
+  end
+
+  def get_word
+    list_of_words = ["word1", "word2", "word3"].shuffle
+    list_of_words[0]
+  end
 
 end
+
+new_word = WordGuess.new
+puts new_word.word
