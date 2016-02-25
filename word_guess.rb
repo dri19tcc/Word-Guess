ccbb7f78c1769c8c27d450fdeb7fb0095886e5a3
Adriana <dri19tcc@gmail.com>
2016-02-25 14:29:57 -0800

    Added new method, and @used_letters instance variable with an empty array.  Pushed used letters into the empty array.  Added rules, got user letter input.

--- word_guess.rb
+++ word_guess.rb
@@ -1,16 +1,30 @@
-class WordGuess 
+class WordGuess
   attr_reader :word
 
   def initialize
     @word = get_word
+    @used_letters = [] #empty array to hold used letters
   end
 
-  def get_word
-    list_of_words = ["word1", "word2", "word3"].shuffle
-    list_of_words[0]
+  def get_word #using this method to use in initialize to call array
+    list_of_words = ["word1", "word2", "word3"].shuffle #shuffle the array so it's different each time
+    list_of_words[0] #always picking first word in array
+  end
+
+  def add_to_used_letters(letter)
+    @used_letters << letter
+    puts "You've used the following letters:  #{@used_letters}."
   end
 
 end
 
-new_word = WordGuess.new
-puts new_word.word
+new_game = WordGuess.new
+#puts new_word.word
+
+puts "We are going to play a game.  We will 'think' " +
+"of a word, and you try to guess the word letter by letter."
+puts "Please give me a letter."
+letter = gets.chomp.downcase #downcase the letter so always matches
+puts "You chose #{letter}." #so user sees what letter they chose
+
+new_game.add_to_used_letters(letter) #new word is our game
