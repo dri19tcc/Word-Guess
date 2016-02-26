1b425626cee384c0676cc62363b0e0c408a6b640
Lisa Rolczynski <lisa.sano@gmail.com>
2016-02-25 16:20:57 -0800

    Added dashes instance variable.  Added dashes_array method. Refactored the display_word method.

--- word_guess.rb
+++ word_guess.rb
@@ -3,12 +3,21 @@ class WordGuess
 
   def initialize
     @word = get_word
+    @dashes = dashes_array
     @used_letters = [] #empty array to hold used letters
+
+    print @word
   end
 
   def get_word #using this method to use in initialize to call array
     list_of_words = ["word1", "word22", "word333"].shuffle #shuffle the array so it's different each time
     word_array = list_of_words[0].split(//) #always picking first word in array
+  end
+
+  def dashes_array
+    length = "_" * @word.length
+    dash_array = length.split(//)
+    return dash_array
   end
 
   def add_to_used_letters(letter)
@@ -17,13 +26,15 @@ class WordGuess
   end
 
   def display_word(letter = "")
-    puts "_ " * @word.length
-    @word.each do |i|
-      if i == letter
-        i = letter
-        puts "letter is changed to " + i
+    @word.each_with_index do |element, index|
+      if element == letter
+        @dashes[index] = letter
       end
     end
+    @dashes.each do |i|
+      print i + " "
+    end
+    puts ""
   end
 
 end
@@ -35,6 +46,7 @@ puts "We are going to play a game.  We will 'think' " +
 "of a word, and you try to guess the word letter by letter."
 puts "Please give me a letter."
 new_game.display_word
+
 letter = gets.chomp.downcase #downcase the letter so always matches
 puts "You chose #{letter}." #so user sees what letter they chose
 
